138c355eb201123bdbb24ff65ff5b1447730946c
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-10-24 03:28:36 +0600

    Update deepClone method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 function getDataType(data) {
   return Object.prototype.toString.call(data).slice(8, -1);
@@ -8,14 +8,14 @@ function isCyclic(data) {
   let seenObjects = [];
 
   function detect(data) {
-    if (data && getDataType === 'object') {
+    if (data && getDataType(data) === "Object") {
       if (seenObjects.indexOf(data) !== -1) {
         return true;
       }
 
       seenObjects.push(data);
 
-      for(var key in data) {
+      for (var key in data) {
         if (data.hasOwnProperty(key) === true && detect(data[key])) {
           return true;
         }
@@ -23,20 +23,50 @@ function isCyclic(data) {
     }
     return false;
   }
+
   return detect(data);
 }
 
-const deepClone = (data) => {
+const deepClone = function(data) {
   if (data === null || data === undefined) {
     return undefined;
   }
 
   const dataType = getDataType(data);
 
-  if (dataType === 'Date') {
-    let cloneDate = new Date();
-    cloneDate.setTime(data.getTime());
+  if (dataType === "Date") {
+    let clonedDate = new Date();
+    clonedDate.setTime(data.getTime());
 
-    return cloneDate;
+    return clonedDate;
   }
-}
+
+  if (dataType === "Object") {
+    if (isCyclic(data) === true) {
+      return data;
+    }
+
+    let copiedObject = {};
+
+    // Iterate over the objects keys
+    for (let key in data) {
+      copiedObject[key] = deepClone(data[key]);
+    }
+
+    return copiedObject;
+  }
+
+  if (dataType === "Array") {
+    let copiedArray = [];
+
+    for (var i = 0; i < data.length; i++) {
+      copiedArray.push(deepClone(data[i]));
+    }
+
+    return copiedArray;
+  } else {
+    return data;
+  }
+};
+
+module.exports = deepClone;
